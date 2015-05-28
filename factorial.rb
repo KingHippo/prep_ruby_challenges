fcd9420ecddc5afe1ed1f0ef91f78877367aa4cd
Don Price <askdonno@gmail.com>
2015-05-28 12:07:22 -0400

    refactor each do block

--- factorial.rb
+++ factorial.rb
@@ -1,11 +1,9 @@
 def factorial(num)
-        if (num == 0 || num == !)
+        if num == 0 || num == 1
             puts "1"
         end
         count = 1
-        (1..num).each do |n|
-            count *= n
-        end
+        (1..num).each {|n| count *= n}
         puts count.to_s
 end
 
