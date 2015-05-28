42ce8d6a3d306945514b9b5ff1ebe2df045ad70f
Don Price <askdonno@gmail.com>
2015-05-28 12:58:04 -0400

    refactor puts method

--- primes.rb
+++ primes.rb
@@ -1,8 +1,8 @@
 def primes(num)
-    if num / 2 != 0
-        puts "#{num}" + " is prime"
+    if num /2 != 0
+      puts "#{num} is prime"
     else
-        puts "#{num}" + " is not prime"
+      puts "#{num} is not prime"
     end
 end
 
