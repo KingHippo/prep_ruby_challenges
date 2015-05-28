58cd93ecd8183fc872f67e9b7a27b65399912b1f
Don Price <askdonno@gmail.com>
2015-05-28 12:27:10 -0400

    refactor unless statement

--- uniques.rb
+++ uniques.rb
@@ -1,10 +1,9 @@
 def uniques(items)
     array = []
-    items.each do |item|
-        unless array.include?(item)
-          array << item
-        end
-    end
+    items.each {|item| unless array.include?(item)
+                array << item
+                end}
+
     puts array.to_s
 end
 
